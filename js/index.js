5339bc6b7e5324261656643ff8af03f7acd56cc2
momodouksowe <momodouksowe@gmail.com>
2016-10-19 17:56:49 +0100

    Corrected errors on JavaScript file and Ashesi file

--- js/index.js
+++ js/index.js
@@ -4,40 +4,50 @@
 	var destinationType;
 	var pictureSource;
 
+
 	function onDeviceReady(){
+		alert("we are here");
 		pictureSource = navigator.camera.pictureSource;
 		destinationType = navigator.camera.destinationType;
 
-	
+	}
+
+
  	document.getElementById('capturePhoto').onclick = function(){
  		alert("Am here!");
 		navigator.camera.getPicture(onPhotoDataSuccess, onFail,{
 		quality : 50, destinationType : destinationType.DATA_URL
   });
- }
+ };
+
     document.getElementById('geolocation').onclick = function(){
  		alert("Am here!");
-		var watchID = navigator.geolocation.watchPosition(onSuccess,onError);
- }
+		var watchID = navigator.geolocation.watchPosition(onSuccess,onError, {timeout:5000});
+ };
 
-};
+
  function onPhotoDataSuccess(imagedata){
 	var smallImage = document.getElementById('smallImage');
 	smallImage.style.display ='block';
 	smallImage.src="data:image/jpeg;base64," +imagedata;
 }
+
   function onFail(message){
 	alert("Failed because: "+ message);
   }
+
   function onSuccess(position){
-  	var element = document.getElementById('geolocation');
-  	element.innerHTML = 'Latitude:' + position.coords.Latitude+'<br>'
-  					    'Longitude: '+ position.coords.Latitude + '<br>'
-  					    '<hr /> '+ element.innerHTML;
+  	//var element = document.getElementById('geolocation');
+  	alert('Latitude:' + position.coords.Latitude +'<br>'+
+  					    'Longitude: '+ position.coords.Latitude + '<br>'+
+  					    '<hr /> ');
+  					    
   }
+
   function onError(error){
   	alert('code: '+ error.code + '\n'+ 'message: '+error.message+'\n');
   }
+
   // var watchID = navigator.geolocation.watchPosition(onSuccess,onError,timeout);
 }) ();
 // 	function onPhotoURLSuccess(imageURL){
