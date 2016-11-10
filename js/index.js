77e12140467daed9e5bfc923bae9bc4252cfafcb
momodouksowe <momodouksowe@gmail.com>
2016-11-10 14:33:19 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,55 @@
-(function(){
-	
-	document.addEventListener("deviceready", onDeviceReady.bind(this),false);
-	var destinationType;
+(function() {
+
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
-
-
-	function onDeviceReady(){
+	var destinationType;
+	function onDeviceReady() {
 		pictureSource = navigator.camera.PictureSourceType;
 		destinationType = navigator.camera.DestinationType;
-    document.getElementById('capturePhoto').onclick = function(){MyCamera();};
+
+		document.getElementById("capturePhoto").onclick = function() {
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+				quality : 50,
+
+				destinationType : destinationType.DATA_URL
+			});
+		};
+		
+		document.getElementById("geolocationdata").addEventListener("click", function () {
+            navigator.geolocation.getCurrentPosition(onSuccess, onError,{ enableHighAccuracy: true });
+        });  
+		  
+	
 	}
 
- 	
-  function MyCamera(){
-  	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50,
-        destinationType: destinationType.DATA_URL });
-  }
+	function onPhotoDataSuccess(imageData) {
 
-    document.getElementById('geolocation').addEventListener("click",function(){
-		navigator.geolocation.getCurrentPosition(onSuccess,onError, {enableHighAccuracy: true
-	});
- });
+		var smallImage = document.getElementById('smallImage');
 
+		smallImage.style.display = 'block';
 
- function onPhotoDataSuccess(imagedata){
-	var smallImage = document.getElementById('smallImage');
-	smallImage.style.display ='block';
-	smallImage.src="data:image/jpeg;base64," +imagedata;
-}
+		smallImage.src = "data:image/jpeg;base64," + imageData;
 
-  function onFail(message){
-	alert("Failed because: "+ message);
-  }
+	}
 
-  function onSuccess(position){
-  	//var element = document.getElementById('geolocation');
-  	alert('Latitude:' + position.coords.latitude +'\n'+
-  					    'Longitude: '+ position.coords.longitude + '\n');
-  					    
-  }
+	function onFail(message) {
 
-  function onError(error){
-  	alert('code: '+ error.code + '\n'+ 'message: '+error.message+'\n');
-  }
+		alert('Failed because: ' + message);
 
+	}
 
-}) ();
+	
+	///////////geolocation bit/////////////////
+	  var onSuccess = function (position) {
+            alert('Latitude: ' + position.coords.latitude + '\n' +
+                  'Longitude: ' + position.coords.longitude + '\n' 
+                 );
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: ' + error.code + '\n' +
+                  'message: ' + error.message + '\n');
+        }
+})();
